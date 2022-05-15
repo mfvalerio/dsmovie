812b35279d9bb38b3a3c2ae122bd7da05f936e1a
Mariana Valério <mariana.dfvalerio@gmail.com>
2022-05-15 18:04:04 -0300

    _redirects

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -38,6 +38,7 @@ function Form() {
                 <Link to="/">
                     <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
                 </Link>
+                
             </div >
         </div >
     );
