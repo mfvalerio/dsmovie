f978b01213898ce07816f48ca3b1ce751bb7b184
Mariana Valério <mariana.dfvalerio@gmail.com>
2022-05-15 20:17:23 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,25 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { BASE_URL } from "utils/requests";
+import { MoviePage } from "types/movie" ;
 
 function Listing() {
+
+    const [pageNumber, setPageNumber] = useState(0);
+
+   
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies`).then(response => {
+            const data = response.data as MoviePage;
+            console.log(data);
+            setPageNumber(data.number);
+        });
+    }, []);
+
+
     return (
         <>
             <Pagination />
